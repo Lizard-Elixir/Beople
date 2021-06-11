1517df44040ae511cf4c3ab3c2d02db634f483cc
Alex Van Camp <vancamp.alex@gmail.com>
2021-06-11 15:59:47 -0500

    Basic movement and camera

---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+	public Transform target;
+
+	private Vector3 offset;
+
+	void Start()
+	{
+		offset = transform.position - target.position;
+	}
+
+	void LateUpdate()
+	{
+		transform.LookAt(target);
+		transform.position = target.position + offset;
+	}
+}
