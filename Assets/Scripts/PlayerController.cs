b61df6bb3d3ad3e789a94f6848e7d4002affeef4
Alexander Lange Van Camp <vancamp.alex@gmail.com>
2021-06-12 10:05:01 -0500

    feat(beople): add special gameplay effects when recruiting leaders (#7)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,13 +9,19 @@ public class PlayerController : MonoBehaviour
 
 	private GameObject SquawkGfx;
 	private float LastSquawkTime;
+	[SerializeField] private VarObject recruitedBeopleVar;
 
 	// Start is called before the first frame update
 	void Start()
 	{
 		SquawkGfx = GameObject.Find("Squawk");
-		SquawkGfx.transform.localScale = new Vector3(SquawkRadius * 2, SquawkGfx.transform.localScale.y, SquawkRadius * 2);
+		SetSquawkRadius(SquawkRadius);
 		SquawkGfx.SetActive(false);
+	}
+
+	void SetSquawkRadius(float radius)
+	{
+		SquawkGfx.transform.localScale = new Vector3(SquawkRadius * 2, SquawkGfx.transform.localScale.y, SquawkRadius * 2);
 	}
 
 	// Update is called once per frame
@@ -51,10 +57,31 @@ public class PlayerController : MonoBehaviour
 			float distance = Vector3.Distance(transform.position, berson.transform.position);
 			if (distance <= SquawkRadius)
 			{
-				berson.GetComponent<BirdController>().Recruit();
+				bool recruited = berson.GetComponent<BirdController>().Recruit();
+				if (recruited)
+				{
+					HandleRecruitment(berson);
+				}
 			}
 		}
 
 		LastSquawkTime = Time.time;
+	}
+
+	void HandleRecruitment(GameObject berson)
+	{
+		switch (berson.name)
+		{
+			case "ChickenLeader":
+				recruitedBeopleVar.currentNum += 10;
+				break;
+			case "SparrowLeader":
+				ThirdPersonMovement movementScript = GetComponentInParent<ThirdPersonMovement>();
+				movementScript.speed *= 2f;
+				break;
+			case "MagpieLeader":
+				SetSquawkRadius(SquawkRadius *= 1.5f);
+				break;
+		}
 	}
 }
