f32163663446bb88a0a95e1898c7ea72646991b7
Katherine Russell <71149848+KatRuss@users.noreply.github.com>
2021-06-12 17:29:07 +0100

    numbers on walls

--- Assets/Scripts/BirdController.cs
+++ Assets/Scripts/BirdController.cs
@@ -41,6 +41,7 @@ public class BirdController : MonoBehaviour
 		recruitedBeopleVar.currentNum += 1;
 		Debug.Log("Recruited Berson!");
 		Debug.Log(recruitedBeopleVar.currentNum);
+		return true;
 	}
 
 	void MoveTowardsPlayer()
