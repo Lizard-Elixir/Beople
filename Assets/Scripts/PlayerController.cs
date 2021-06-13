2ace4aad30610ed90b5219bf8bcb8653e0fec015
Katherine Russell <71149848+KatRuss@users.noreply.github.com>
2021-06-13 11:59:19 +0100

    level stuff and wall changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -105,4 +105,5 @@ public class PlayerController : MonoBehaviour
 	{
 		Talking = isTalkingNow;
 	}
+
 }
--- Assets/Scripts/WallController.cs
+++ Assets/Scripts/WallController.cs
@@ -14,27 +14,22 @@ public class WallController : MonoBehaviour
         requirementHUD.text = beopleRequirement.ToString();
     }
 
-    private void Update()
-    {
-        if (recruitedBeopleVar.currentNum >= beopleRequirement)
-        {
-            Destroy(gameObject);
-        }
-    }
 
 
-    /** for some reason OnCoillisionEnter isn't working?
+    // for some reason OnCoillisionEnter isn't working?
     //When gets bumped by something, checks if it was the player, then if they have enough people it is destroyed
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("has been collided");
         if (collision.gameObject.tag == "Player")
         {
             if (recruitedBeopleVar.currentNum >= beopleRequirement)
             {
+                GameObject particles = Instantiate(breakParticles);
+                particles.transform.position = gameObject.transform.position;
+                particles.GetComponent<ParticleSystem>().Play();
+                Destroy(particles,1000);
                 Destroy(gameObject);
             }
         }
     }
-    */
 }
