c3a78a4b4db738b60534b10479c92fbf3442df89
Alex Van Camp <vancamp.alex@gmail.com>
2021-06-15 17:09:26 -0500

    style: format WallController.cs

--- Assets/Scripts/WallController.cs
+++ Assets/Scripts/WallController.cs
@@ -4,35 +4,35 @@ using UnityEngine;
 using TMPro;
 public class WallController : MonoBehaviour
 {
-    [SerializeField] float beopleRequirement = 5f;
-    [SerializeField] private VarObject recruitedBeopleVar;
-    [SerializeField] private TextMeshProUGUI requirementHUD;
-    [SerializeField] private GameObject breakParticles;
+	[SerializeField] float beopleRequirement = 5f;
+	[SerializeField] private VarObject recruitedBeopleVar;
+	[SerializeField] private TextMeshProUGUI requirementHUD;
+	[SerializeField] private GameObject breakParticles;
 
-    private void Start()
-    {
+	private void Start()
+	{
 
-    }
+	}
 
-    private void Update()
-    {
-        requirementHUD.text =  recruitedBeopleVar.currentNum + "/" + beopleRequirement.ToString();
-    }
+	private void Update()
+	{
+		requirementHUD.text = recruitedBeopleVar.currentNum + "/" + beopleRequirement.ToString();
+	}
 
-    // for some reason OnCoillisionEnter isn't working?
-    //When gets bumped by something, checks if it was the player, then if they have enough people it is destroyed
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            if (recruitedBeopleVar.currentNum >= beopleRequirement)
-            {
-                GameObject particles = Instantiate(breakParticles);
-                particles.transform.position = gameObject.transform.position;
-                particles.GetComponent<ParticleSystem>().Play();
-                Destroy(particles,1000);
-                Destroy(gameObject);
-            }
-        }
-    }
+	// for some reason OnCoillisionEnter isn't working?
+	//When gets bumped by something, checks if it was the player, then if they have enough people it is destroyed
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.tag == "Player")
+		{
+			if (recruitedBeopleVar.currentNum >= beopleRequirement)
+			{
+				GameObject particles = Instantiate(breakParticles);
+				particles.transform.position = gameObject.transform.position;
+				particles.GetComponent<ParticleSystem>().Play();
+				Destroy(particles, 1000);
+				Destroy(gameObject);
+			}
+		}
+	}
 }
