cb1c9283cea8abbfa0f30ea12f8da9877d6932e7
Alex Van Camp <vancamp.alex@gmail.com>
2021-06-16 17:45:18 -0500

    feat(graphics): fade in frost overlay as timer counts down
    Current behavior is that the frost overlay begins fading in at 45 seconds remaining and is fully faded in at 15 seconds remaining. Both of these numbers are adjustable.

---
+++ Assets/Scripts/FrostOverlay.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FrostOverlay : MonoBehaviour
+{
+	[Tooltip("Time (in seconds) when the frost graphic should start fading in.")]
+	public int frostBeginTime = 45;
+	[Tooltip("Time (in seconds) when the frost graphic should fully faded in.")]
+	public int frostCompleteTime = 15;
+	[SerializeField] VarNumber timer;
+	private Image image;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		image = GetComponent<Image>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Color tempColor = image.color;
+		if (timer.currentNum > frostBeginTime)
+		{
+			tempColor.a = 0;
+		}
+		else
+		{
+			tempColor.a = 1 - (timer.currentNum - frostCompleteTime) / frostBeginTime;
+		}
+
+		image.color = tempColor;
+	}
+}
