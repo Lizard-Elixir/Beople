1f17903a302cd5d70b8af6c81bb3af516a0b786e
Alex Van Camp <vancamp.alex@gmail.com>
2021-06-18 11:39:00 -0500

    feat(gameplay): add speech bubble above leader heads when in chat radius

--- Assets/Scripts/BirdLeaderController.cs
+++ Assets/Scripts/BirdLeaderController.cs
@@ -4,10 +4,50 @@ using UnityEngine;
 
 public class BirdLeaderController : BirdController
 {
-    public Dialogue dialogue;
+	public Dialogue dialogue;
+	private SquawkController SquawkController;
+	private GameObject SpeechBubble;
 
-    public void TriggerDialogue()
-    {
-        FindObjectOfType<DialogueManager>().StartDialogue(dialogue, this);
-    }
+	new void Start()
+	{
+		base.Start();
+		SquawkController = player.transform.Find("Squawk").gameObject.GetComponent<SquawkController>();
+		SpeechBubble = transform.Find("GFX").transform.Find("SpeechBubble").gameObject;
+	}
+
+	new void Update()
+	{
+		base.Update();
+		if (IsRecruited)
+		{
+			HideSpeechBubble();
+		}
+		else
+		{
+			float distanceFromPlayer = Vector3.Distance(transform.position, player.transform.position);
+			if (distanceFromPlayer <= SquawkController.SquawkRadius)
+			{
+				ShowSpeechBubble();
+			}
+			else
+			{
+				HideSpeechBubble();
+			}
+		}
+	}
+
+	private void ShowSpeechBubble()
+	{
+		SpeechBubble.SetActive(true);
+	}
+
+	private void HideSpeechBubble()
+	{
+		SpeechBubble.SetActive(false);
+	}
+
+	public void TriggerDialogue()
+	{
+		FindObjectOfType<DialogueManager>().StartDialogue(dialogue, this);
+	}
 }
