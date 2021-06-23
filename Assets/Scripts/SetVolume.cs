4c6720f6cff22f78401bc4f4994c91005c451e52
Alex Van Camp <vancamp.alex@gmail.com>
2021-06-23 01:26:40 -0500

    feat(UI): save volume to PlayerPrefs

--- Assets/Scripts/SetVolume.cs
+++ Assets/Scripts/SetVolume.cs
@@ -2,13 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+
 public class SetVolume : MonoBehaviour
 {
 	public AudioMixer mixer;
+	public Slider slider;
+
+	void Start()
+	{
+		slider.value = PlayerPrefs.GetFloat("MainVol", 0.75f);
+	}
 
 	public void SetLevel(float sliderValue)
 	{
 		// https://www.youtube.com/watch?v=xNHSGMKtlv4
 		mixer.SetFloat("MainVol", Mathf.Log10(sliderValue) * 20);
+		// https://gamedevbeginner.com/the-right-way-to-make-a-volume-slider-in-unity-using-logarithmic-conversion/#playpref
+		PlayerPrefs.SetFloat("MainVol", sliderValue);
 	}
 }
